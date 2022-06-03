904986e0b77fc7369890309ffcd7d2a0ee1d4434
Gustavo Ferreira <gustavofrsnt@gmail.com>
2022-06-03 11:27:46 -0300

    Responsividade-V1
    Adicionando responsividade na section "capa".

--- responsividade.js
+++ responsividade.js
@@ -1,10 +1,22 @@
 const mobileResponsiv = document.getElementById("menu-mobile");
 
-function toggleMobile(){
+function toggleMobile(e){
+
+    if (e.type === 'touchstart') e.preventDefault();
     const navegacao = document.getElementById("nav");
 
     navegacao.classList.toggle("active");
     mobileResponsiv.classList.toggle("active");
+    const active = navegacao.classList.contains('active')
+    e.currentTarget.setAttribute('aria-expanded', active);
+    if (active) {
+        e.currentTarget.setAttribute('aria-label', "Fechar Menu");
+    }else {
+        e.currentTarget.setAttribute('aria-label', "Abrir Menu");
+    }
+
+
 }
 
 mobileResponsiv.addEventListener('click', toggleMobile);
+mobileResponsiv.addEventListener('touchstart', toggleMobile);
