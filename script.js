9cb0a7e262e5919f5f76e6f9e3c6c02099bd5ac7
Gustavo Ferreira <gustavofrsnt@gmail.com>
2022-06-27 00:35:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function enviarEmail(){
         message: document.getElementById("input-escreva").value
     };
     if (document.getElementById("input-email").value=="" ||  document.getElementById("input-escreva").value==""){
-        alert("Preencha todos os campos!");
+        alert("There are empty inputs! Please, insert all the needed informations.");
     }else (
         emailjs.send("service_nr8jdh9", "template_qu6go4a", params).then(function (res){
-            document.getElementById("desc-contatos").innerHTML='Email enviado!';
+            document.getElementById("desc-contatos").textContent='Email sended!';
             document.getElementById("input-email").value='';
             document.getElementById("input-escreva").value='';
         })
